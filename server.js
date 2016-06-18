d04eae691d182cf21f16236bf0039d24ead7df98
Jake Boyles <jake@jibdesigns.com>
2016-06-18 15:29:26 -0400

    html, gulp, some other shit

--- server.js
+++ server.js
@@ -4,16 +4,21 @@ const session = require('express-session');
 const initializePassport = require('./passport');
 const routes = require('./routes');
 const server = express();
+var cons = require('consolidate');
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
+
+server.engine('html', cons.swig)
+
 server.use(session({
   secret: process.env.APP_SECRET,
   resave: false,
   saveUninitialized: false,
 }));
-server.set('views', './views');
-server.set('view engine', 'pug');
+
+server.set('views', 'views');
+server.set('view engine', 'html');
 server.use(express.static('public'));
 server.use(initializePassport());
 
