92c9f3d96873e6b5345ba5c70f34d162caa40333
Alex Sears <alexwsears@gmail.com>
2016-06-18 14:25:23 -0400

    Fixed PORT name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const server = require('./server');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log('The CincyBrewScene server is running.');
