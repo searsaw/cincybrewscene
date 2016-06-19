019cf458d053e0814b912a2bde90bb71dc1e6cda
Alex Sears <alexwsears@gmail.com>
2016-06-19 03:18:10 -0400

    Got route painted on screen

--- public/js/find_directions.js
+++ public/js/find_directions.js
@@ -93,55 +93,6 @@ function getGeomappingDataFromArcGis(Geoprocessor, Point, data) {
 }
 
 function setupGeomapping(Geoprocessor, Point) {
-  const data = [{
-      "_id" : "5765f170bad0f48e9605cdc4",
-      "name" : "Fibonacci Brewing Company",
-      "address" : "1445 Compton Rd",
-      "city" : "Cincinnati",
-      "state" : "OH",
-      "zip" : "45231",
-      "phone" : "5138321422",
-      "loc" : [
-          -84.540985,
-          39.231448
-      ],
-      "website" : "http://www.fibbrew.com",
-      "twitter" : "https://twitter.com/fibbrewing",
-      "facebook" : "https://www.facebook.com/fibonaccibrewing",
-      "logo" : "http://d3d27zyflwslfn.cloudfront.net/images/fibonacci.jpeg"
-  }, {
-      "_id" : "5765f170bad0f48e9605cdc8",
-      "name" : "Mt. Carmel Brewing Company",
-      "address" : "4362 Mt. Carmel-Tobasco Rd.",
-      "city" : "Cincinnati",
-      "state" : "OH",
-      "zip" : "45244",
-      "phone" : "5132402739",
-      "loc" : [
-          -84.300243,
-          39.093579
-      ],
-      "website" : "http://www.mtcarmelbrewingcompany.com",
-      "twitter" : "https://twitter.com/MtCarmelBrewing",
-      "facebook" : "https://www.facebook.com/mtcarmelbrewing",
-      "logo" : "http://d3d27zyflwslfn.cloudfront.net/images/mtcarmel.png"
-  }, {
-      "_id" : "5765f170bad0f48e9605cdcf",
-      "name" : "Urban Artifact",
-      "address" : "1660 Blue Rock St",
-      "city" : "Cincinnati",
-      "state" : "OH",
-      "zip" : "45223",
-      "phone" : "5136204729",
-      "loc" : [
-          -84.54202,
-          39.160671
-      ],
-      "website" : "http://www.artifactbeer.com",
-      "twitter" : "https://twitter.com/ArtifactBeer",
-      "facebook" : "https://www.facebook.com/urbanartifactbrewing",
-      "logo" : "http://d3d27zyflwslfn.cloudfront.net/images/urbanartifact.png"
-  }];
   getGeomappingDataFromArcGis(Geoprocessor, Point, data);
 }
 
